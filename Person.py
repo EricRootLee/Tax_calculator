98a04e61e10ced4f7ca83353c23f1bee4e47742f
EricRootLee <erickiokom@gmail.com>
2021-08-13 14:28:28 +0300

    Moved calculator to model

--- Person.py
+++ Person.py
@@ -5,7 +5,15 @@ class Person:
         self.salary = salary
         self.net_pay = net_pay
 
+    def calc_tax(self, tax) -> int:
+        income_tax = self.salary * (tax / 100)
+        net_pay = self.salary - income_tax
+        return net_pay
 
-
-
-
+    def total_calc_tax(self) -> int:
+        if 0 < self.age <= 45:
+            return self.calc_tax(9)
+        elif 46 <= self.age <= 65:
+            return self.calc_tax(5)
+        else:
+            return self.calc_tax(3)
