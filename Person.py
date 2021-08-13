43e5dfac64b2c3acc3616599565e90b90187092e
EricRootLee <erickiokom@gmail.com>
2021-08-13 14:25:34 +0300

    DOne calc

--- Person.py
+++ Person.py
@@ -1,5 +1,11 @@
 class Person:
-    def __init__(self, name: str, age: int, salary: int):
+    def __init__(self, name: str, age: int, salary: int, net_pay: int):
         self.name = name
         self.age = age
         self.salary = salary
+        self.net_pay = net_pay
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,15 +1,40 @@
 import tax_calculator as tc
+import Person as pc
 
-while True:
+workers = []
+counter = 0
+
+
+def all_people():
+    if len(workers) > 0:
+        print("|No.| Name | Age | Gross Pay | Net Pay |")
+        print("|---|------|-----|-----------|---------|")
+        for worker in workers:
+            print("|", workers.index(worker) + 1, "|", worker.name, "|", worker.age, "|", worker.salary, "|",
+                  worker.net_pay, "|")
+    else:
+        print("No Workers")
+
+
+while counter < 2:
     try:
         name = str(input("Enter your name: \n"))
         try:
             age = int(input("Enter your age: \n"))
             gross_pay = int(input("Enter your pay: \n"))
             net_pay = tc.total_calc_tax(gross_pay, age)
-            print(name, "your take away home salary is ,", "Ksh:", net_pay)
+            person = pc.Person(
+                name,
+                age,
+                gross_pay,
+                net_pay
+            )
+            workers.append(person)
+            counter += 1
         except ValueError:
             print("Invalid Data")
     except KeyboardInterrupt:
         print('The program was stopped by the user')
+        all_people()
         exit()
+all_people()
